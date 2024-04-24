e10773901ce316cb0eba7936a150779ff1d38a6f
noscha <noah.schager@t-online.de>
2024-04-24 17:52:05 +0200

    skeleton task2/3

---
+++ code/utils/search.py
@@ -0,0 +1,9 @@
+def bfs(adv = False):
+    ''' Simple Bfs. If advanced is true, trivial deadlock states will be ignored '''
+    pass
+
+def dfs(adv = False):
+    pass
+
+def idfs(adv = False):
+    pass
